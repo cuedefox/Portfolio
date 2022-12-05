764424630b03c92d2511015ec7ef03ba383e1aa3
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2022-12-05 05:13:42 -0300

    agrego responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import { React, useState } from "react";
 import en from '../assets/img/reino-unido.png';
 import es from '../assets/img/espana.png';
 
 const NavBar = () => {
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    const desplegarMenu = () => {
+        setMobileMenu(!mobileMenu);
+    };
+
     return <div className="nav-bar">
         <div>
             <p className="nav-logo">
                 <span>{'{'}</span>Rodrigo<span>:</span> Vergara<span>{'}'}</span>
             </p>
             <nav>
-                <ul>
+                <ul className={mobileMenu ? "menu-items show" : "menu-items"}>
                     <li><a href="#header">Home</a></li>
                     <li><a href="#about-me">Sobre Mi</a></li>
                     <li><a href="#skills">Habilidades</a></li>
@@ -17,6 +23,11 @@ const NavBar = () => {
                     <li><a href="#contact">Contacto</a></li>
                     <li className="li-idioma"><div><img src={en} alt="boton idioma" /></div></li>
                 </ul>
+                <div className="menu-bars" onClick={desplegarMenu}>
+                    <span className={mobileMenu ? "activelinea1-menu-bar" : "linea1-menu-bar"}></span>
+                    <span className={mobileMenu ? "activelinea2-menu-bar" : "linea2-menu-bar"}></span>
+                    <span className={mobileMenu ? "activelinea3-menu-bar" : "linea3-menu-bar"}></span>
+                </div>
             </nav>
         </div>
     </div>
