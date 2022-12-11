28b416fdc76c9a155dfe71e860edee857e166959
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2022-12-11 19:29:37 -0300

    agrego skill

--- src/db/skills.js
+++ src/db/skills.js
@@ -15,7 +15,8 @@ const skills = [
     {id: 14, title: "WEBPACK", description: "asdasfasda", img: "webpack-original.svg"},
     {id: 15, title: "TRELLO", description: "sadasdadasd", img: "trello-plain.svg"},
     {id: 16, title: "MYSQL", description: "sadasdadasd", img: "mysql-original-wordmark.svg"},
-    {id: 17, title: "MARIA DB", description: "sadasdadasd", img: "mariadb.svg"}
+    {id: 17, title: "MARIA DB", description: "sadasdadasd", img: "mariadb.svg"},
+    {id: 17, title: "SQLITE", description: "sadasdadasd", img: "sqlite-original.svg"},
 ]
 
 export default skills;
