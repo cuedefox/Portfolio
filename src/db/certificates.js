c339b8397d07e052b247ae29cdbd859bdd5d133d
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-01-17 21:05:55 -0300

    agrego links

--- src/db/certificates.js
+++ src/db/certificates.js
@@ -3,10 +3,10 @@ const certificates = [
     {id: 2, titleEs: "Javascript", titleEn: "Javascript", img: "javascript.png", institution: "Coderhouse", descriptionEs: "", descriptionEn: "", link: "https://www.coderhouse.com/certificados/62f46cf0817a100024257b42"},
     {id: 3, titleEs: "React Js", titleEn: "React Js", img: "react.png", institution: "Coderhouse", descriptionEs: "", descriptionEn: "", link: "https://www.coderhouse.com/certificados/639106fd5713fa000eabfeff"},
     {id: 4, titleEs: "Carrera Frontend React", titleEn: "React Frontend Career", img: "car-front-react.png", institution: "Coderhouse", descriptionEs: "", descriptionEn: "", link: "https://www.coderhouse.com/certificados/639106fd5713fa000eabff02"},
-    {id: 5, titleEs: "Introduccion a la programacion", titleEn: "Introduction to Programming", img: "introduccion-a-la-programacion.png", institution: "Open Bootcamp", descriptionEs: "", descriptionEn: "", link: "https://community.open-bootcamp.com/user/cuede/certificaciones/b9988cb0-bc96-416a-9add-fad5e1d0e8bf"},
-    {id: 6, titleEs: "Conceptos de la programacion", titleEn: "Programming Concepts", img: "conceptos-de-la-programacion.png", institution: "Open Bootcamp", descriptionEs: "", descriptionEn: "", link: "https://community.open-bootcamp.com/user/cuede/certificaciones/309aca04-1c72-452c-b451-9ce19813f86b"},
-    {id: 7, titleEs: "HTML y CSS",  titleEn: "HTML and CSS", img: "html-y-css.png", institution: "Open Bootcamp", descriptionEs: "", descriptionEn: "", link: "https://community.open-bootcamp.com/user/cuede/certificaciones/ad3b5dda-5d4c-47a0-9c1a-b4227d00e560"},
-    {id: 8, titleEs: "Javascript", titleEn: "Javascript", img: "javascript-ob.png", institution: "Open Bootcamp", descriptionEs: "", descriptionEn: "", link: "https://community.open-bootcamp.com/user/cuede/certificaciones/f42f9bfc-f002-4306-8f65-a5c39f3315b6"}
+    {id: 5, titleEs: "Introduccion a la programacion", titleEn: "Introduction to Programming", img: "introduccion-a-la-programacion.png", institution: "Open Bootcamp", descriptionEs: "", descriptionEn: "", link: "https://drive.google.com/file/d/1rith56UzMJAw87xaXJeZZcVYyXGXJ4LK/view"},
+    {id: 6, titleEs: "Conceptos de la programacion", titleEn: "Programming Concepts", img: "conceptos-de-la-programacion.png", institution: "Open Bootcamp", descriptionEs: "", descriptionEn: "", link: "https://community.open-bootcamp.com/user/cuede/certificaciones/fc4581fe-2482-4e75-ad9f-ec53e539c4d0"},
+    {id: 7, titleEs: "HTML y CSS",  titleEn: "HTML and CSS", img: "html-y-css.png", institution: "Open Bootcamp", descriptionEs: "", descriptionEn: "", link: "https://community.open-bootcamp.com/user/cuede/certificaciones/69ad4828-5604-4bfb-a450-2b1496501331"},
+    {id: 8, titleEs: "Javascript", titleEn: "Javascript", img: "javascript-ob.png", institution: "Open Bootcamp", descriptionEs: "", descriptionEn: "", link: "https://drive.google.com/file/d/1WD5F3H5i4DU62lR-p55nhhz2Lwwl0K2P/view"}
 ]
 
 export default certificates;
