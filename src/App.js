d66a45b107b58955e921263b2386ed4b6211c34c
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-01-22 13:56:51 -0300

    agrego nueva dependencia sass e implemento en el proyecto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './main.css';
+import './scss/import.scss';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
 import MainContainer from './containers/MainContainer';
