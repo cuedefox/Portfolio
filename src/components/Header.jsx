ba066c97bdbd35d9aa38c381082a16b92f5a14b6
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-01-25 23:59:15 -0300

    agrego animacion titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,25 @@ const Header = () => {
 
     return <header id="header" style={{ backgroundImage: `url(${back})`}}>
         {languageEn ? 
-        <h1>Hi!, im <span>Rodrigo Vergara</span>,
-        <br /><span>Full Stack</span> developer.</h1>
-        :
-        <h1>¡Hola!, soy <span>Rodrigo Vergara</span>,
-        <br />desarrollador <span>Full Stack</span>.</h1>
+            <h1>
+                Hi!, im <span className="title-color">Rodrigo Vergara</span>,
+                <br />
+                <div className="texting-container">
+                    <div className="texting">
+                        <span><span className="title-color">Full Stack</span> developer.</span>
+                    </div>
+                </div>
+            </h1>
+            :
+            <h1>
+                ¡Hola!, soy <span className="title-color">Rodrigo Vergara</span>,
+                <br />
+                <div className="texting-container">
+                    <div className="texting">
+                        <span>desarrollador <span className="title-color">Full Stack</span>.</span>
+                    </div>
+                </div>
+            </h1>
         }
         <img src={perfil} alt="Rodrigo Vergara" />
     </header>
