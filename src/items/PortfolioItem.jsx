b01613e986666fdbcd140106285b3589ad4ff872
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-04-03 18:13:53 -0300

    comienzo portfolio

--- src/items/PortfolioItem.jsx
+++ src/items/PortfolioItem.jsx
@@ -3,7 +3,9 @@ import { Language } from "../contexts/Language";
 
 const PortfolioItem = ({item}) => {
     const {languageEn} = useContext(Language);
-    return <div className="portfolio-card">
+    const cargarImagen = require.context("../assets/img/portfolio", true);
+
+    return <div className="portfolio-card" style={{ backgroundImage: `url(${cargarImagen(`./${item.img}`)})`}}>
         <h3>{item.title}</h3>
         <a href={item.link}>{languageEn ? 'See deploy' : 'Ver despliegue'}</a>
         <a href={item.code}>{languageEn ? 'See code' : 'Ver codigo'}</a>
