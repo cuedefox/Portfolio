99d47b1b05bdc3209ddb6cf38715a914d86a88ca
Rodrigo Benjamin Vergara <rodrigovergara2001@gmail.com>
2023-09-10 20:26:08 -0300

    changue wsp number

--- src/components/WhatsappBut.jsx
+++ src/components/WhatsappBut.jsx
@@ -3,7 +3,7 @@ import whatsapp from '../assets/img/icons/whatsapp.png';
 
 const WhatsappBut = () => {
     return <>
-        <a href="https://wa.me/5491168834237?text=Estoy%20interesado%20en%20tus%20servicios%20como%20desarrollador." target="_blank" rel="noreferrer"><img className="but-whatsapp" src={whatsapp} alt="" /></a>
+        <a href="https://wa.me/5491132417239?text=Estoy%20interesado%20en%20tus%20servicios%20como%20desarrollador." target="_blank" rel="noreferrer"><img className="but-whatsapp" src={whatsapp} alt="" /></a>
     </>
 }
 
